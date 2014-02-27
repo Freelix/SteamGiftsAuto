6c37655db00ef893b5c81d74e9748a8ddc232df9
Freelix <felixx.baril@gmail.com>
2014-02-26 22:24:06 -0500

    Add the files (test)

---
+++ background.js
@@ -0,0 +1,126 @@
+/////////////////////////////////
+/////// Global Variables ////////
+/////////////////////////////////
+
+var WISHGAMES = new Array("The Political Machine 2012", "Legacy of Kain: Soul Reaver", "Kung Fu Strike - The Warrior's Rise"); // String array TODO: bind with UI
+var GIFTSPERPAGES = 41;      // number of steam gifts per page
+var NUMBEROFPAGES = 3;		 // the number of pages to analyse
+
+/////////////////////////////////
+///// EventListeners methods ////
+/////////////////////////////////
+
+// Add onclick on the button
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById("btn").addEventListener("click", RequestSteamGifts);
+});
+
+//document.addEventListener("DOMContentLoaded", initDB, false);
+
+/////////////////////////////////
+/////// Primary methods /////////
+/////////////////////////////////
+
+function RequestSteamGifts(){
+	// Connection on the proper website (Page 1)
+	var req = new XMLHttpRequest();
+	req.open("GET", "http://www.steamgifts.com/", true);
+	req.send(null);
+	
+	// Launching the first http request
+	launchRequest(req);
+	
+	for (var ii = 2; ii <= NUMBEROFPAGES; ii++){
+		// Connection on the proper website (Page ii)
+		var addReq = new XMLHttpRequest();
+		addReq.open("GET", "http://www.steamgifts.com/open/page/" + ii, true);
+		addReq.send(null);
+		
+		// Launching the others http requests
+		launchRequest(addReq);
+	}
+}
+
+function launchRequest(req){
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200){
+			var webPage = req.responseText;
+				
+			// Convert string to DOM
+			var parser = new DOMParser();
+			var xmlDoc = parser.parseFromString(webPage,"text/html");
+				
+			// Iterate through DOM
+			searchingForGames(xmlDoc);
+		}
+	}
+}
+
+function searchingForGames(xmlDoc){
+	var nodes = xmlDoc.getElementsByClassName("title");
+	var urls = new Array();
+	var tabArray = new Array();
+	var urlCount = 0;
+	
+	for (var i = 0; i < GIFTSPERPAGES; i++){
+		var element = nodes[i].getElementsByTagName("a");
+		
+		if ($.inArray(element[0].innerHTML.replace("&amp;","&"), WISHGAMES) > -1){
+			console.log(element[0].innerHTML);
+			var url = element[0].getAttribute("href");
+			urls[urlCount] = url;
+			urlCount = urlCount + 1;
+		}
+	}
+	
+	for (var j = 0; j < urls.length; j++){
+		chrome.tabs.create({
+			url: "http://www.steamgifts.com" + urls[j],
+			selected: false
+		}, function(tab){
+			chrome.tabs.executeScript(tab.id, {
+				code: 'if (document.getElementsByClassName("rounded view submit_entry")[0] != null) {' +
+					      'var link = document.getElementsByClassName("rounded view submit_entry")[0].innerHTML;' +
+					      'if (link.startsWith("Enter to Win!"))' +
+						      'document.getElementById("form_enter_giveaway").submit();' +
+					  '}'
+			}, function (result){
+				 setTimeout(function(){chrome.tabs.remove(tab.id);}, 2500);
+			});	
+			
+		});
+	}
+	
+	console.log("End");
+}
+
+/////////////////////////////////
+////// Additionals methods //////
+/////////////////////////////////
+
+// Add a function to the Array object
+Array.prototype.contains = function(obj){
+    var i = this.length;
+    while (i--){
+        if (this[i] === obj){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Check if the string starts with the giving parameter
+// ex: "Someting".startsWith("Lib"); // False
+// ex: "Someting".startsWith("Som"); // True
+if (typeof String.prototype.startsWith != 'function'){
+  String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+  };
+}
+
+// Print the entire page content
+function PrintDOM(xmlDoc){
+	console.log("\n------------------------Start DOM------------------------\n");
+	console.log(xmlDoc);
+	console.log("\n------------------------Stop  DOM------------------------\n");
+}
---
+++ database.js
@@ -0,0 +1,156 @@
+//////////////////
+// Namespace to encapsulate the database logic
+//////////////////
+
+var html5DB = {};
+html5DB.indexedDB  = {};
+
+//////////////////
+// Opening/Creating an Object Store
+//////////////////
+
+html5DB.indexedDB.open = function() {
+  var version = 1;
+  var request = indexedDB.open("wishGamesDB", version);
+
+  // We can only create Object stores in a versionchange transaction.
+  request.onupgradeneeded = function(e) {
+    var db = e.target.result;
+
+    // A versionchange transaction is started automatically.
+    e.target.transaction.onerror = html5DB.indexedDB.onerror;
+
+    if(db.objectStoreNames.contains("wishGames")) {
+      db.deleteObjectStore("wishGames");
+    }
+
+    var store = db.createObjectStore("wishGames",
+      {keyPath: "timeStamp"}); // Property that makes an individual object in the store unique
+  };
+
+  request.onsuccess = function(e) {
+    html5DB.indexedDB.db = e.target.result;
+    html5DB.indexedDB.getAllWishGames();
+  };
+
+  request.onerror = html5DB.indexedDB.onerror;
+};
+
+//////////////////
+// Adding data to an object store
+//////////////////
+
+html5DB.indexedDB.addWishGames = function(newGame) {
+  var db = html5DB.indexedDB.db;
+  var trans = db.transaction(["wishGames"], "readwrite");
+  var store = trans.objectStore("wishGames");
+  var request = store.put({
+    "text": newGame,
+    "timeStamp" : new Date().getTime() // timeStamp property, that is our unique key for the object
+  });
+
+  request.onsuccess = function(e) {
+    html5DB.indexedDB.getAllWishGames();
+  };
+
+  request.onerror = function(e) {
+    console.log(e.value);
+  };
+};
+
+//////////////////
+// Querying the data in a store.
+//////////////////
+
+html5DB.indexedDB.getAllWishGames = function() {
+  var gameList = document.getElementById("wishGamesItems");
+  gameList.innerHTML = "";
+
+  var db = html5DB.indexedDB.db;
+  var trans = db.transaction(["wishGames"], "readwrite");
+  var store = trans.objectStore("wishGames");
+
+  // Get everything in the store
+  var keyRange = IDBKeyRange.lowerBound(0);
+  var cursorRequest = store.openCursor(keyRange);
+
+  // Loop in everything in the store 
+  cursorRequest.onsuccess = function(e) {
+    var result = e.target.result;
+    if(!!result == false)
+      return;
+
+    renderTodo(result);
+    result.continue();
+  };
+
+  cursorRequest.onerror = html5DB.indexedDB.onerror;
+};
+
+//////////////////
+// Rendering data from an Object Store
+//////////////////
+
+function renderTodo(row) {
+  var gameList = document.getElementById("wishGamesItems");
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+  var t = document.createTextNode();
+  t.data = row.value.text;
+  var id = row.key;
+
+  a.textContent = " [Delete]";
+  a.setAttribute("href", "#");
+  li.appendChild(t);
+  li.appendChild(a);
+  gameList.appendChild(li);
+  
+  a.addEventListener("click", function(event) {
+    html5DB.indexedDB.deleteWishGame(id);
+  });
+}
+
+//////////////////
+// Deleting data from a table
+//////////////////
+
+html5DB.indexedDB.deleteWishGame = function(id) {
+  var db = html5DB.indexedDB.db;
+  var trans = db.transaction(["wishGames"], "readwrite");
+  var store = trans.objectStore("wishGames");
+
+  var request = store.delete(id);
+
+  request.onsuccess = function(e) {
+    html5DB.indexedDB.getAllWishGames();  // Refresh the screen
+  };
+
+  request.onerror = function(e) {
+    console.log(e);
+  };
+};
+
+//////////////////
+// Page load
+//////////////////
+
+function initDB() {
+  html5DB.indexedDB.open(); // open and displays the data previously saved
+}
+
+// Add onclick on the button
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById("btnDB").addEventListener("click", addGame);
+});
+document.addEventListener("DOMContentLoaded", initDB, false);
+
+//////////////////
+// Take the data from the user's input
+//////////////////
+
+function addGame() {
+  var gameToAdd = document.getElementById('gameToAdd');
+
+  html5DB.indexedDB.addWishGames(gameToAdd.value);
+  gameToAdd.value = '';
+}
